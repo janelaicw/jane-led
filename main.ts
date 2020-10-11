686b2d53d51b020e1b8dd8135215efaefd958f6a
janelaicw <72684531+janelaicw@users.noreply.github.com>
2020-10-11 15:01:31 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(randint(1, 6))
+})
